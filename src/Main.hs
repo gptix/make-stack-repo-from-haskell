878a8bedd8a2d9109952ff5f91debcb8f604b4fe
Jud Taylor <jud.taylor@gmail.com>
2018-09-13 13:45:37 -0400

    Learned String---->Text.  Defined some parameters. Pseudocoded refactoring of main.

--- src/Main.hs
+++ src/Main.hs
@@ -7,18 +7,41 @@
 -- make a function like proc that takes one 2-tuple (command, [arg]) and evaluates to
 --      proc command [arg] empty
 -- then map that fn over a list of 2-tuples to consolidate many lines of proc...
--- learn how to convert a String to a Text
+-- DONE: learn how to convert a String to a Text --- import qualified Data.Text as T //  (T.pack string)
+
+
 
 module Main where
 
 import Turtle
 
 
--- need to know how to do this.  I want the result of the concatenation be able to be used as an element in the list of 'strings' that are used as args by proc
---gitHubSSHString :: IsString Text
---gitHubSSHString = "git@github.com:" ++ "gptix" ++ "/" ++ "foo"
--- repoName :: Text
--- repoName = "foo"
+--githubUserName :: String
+--githubUserName = "gptix"
+
+--repoName :: String
+--repoName = "foo"
+
+--locaGitDir :: String
+--localGitDir = "/home/gt/gitstuff"
+-- or
+--localGitdire = T.pack "/home/gt/gitstuff"
+
+--gitHubSSHString :: Text
+--gitHubSSHString = T.pack $ concat ["git@github.com:", githubUserName, "/", repoName)
+
+--type ProcCmd = Text
+--type ProcArg = Text
+ 
+--procEmpty MonadIO io => (ProcCmd, [ProcArg]) -> io ExitCode
+--procEmpty (comd, args) = proc comd args (empty :: Shell Line)
+
+--map procEmpty (   cmdArgsPairs :: [  ( ProcComd, [ProcArg] )  ]   )
+--map procEmpty [ ("stack", ["setup"])
+--              , ("git", ["init"]),
+--              , ("git", ["add","."])
+--              , ("git", ["commit","-m","\"Initial commit.\""])]
+
 
 main = do
     cd "/home/gt/gitstuff/"
@@ -33,12 +56,17 @@ main = do
     proc "git" ["push", "origin", "master"] empty 
 
 
-{-
-procEmpty cd args = proc cd args empty
-procEmpty (aCommand,args) = proc aCommand args empty
-
-map procEmpty [("git", ["init"]),
-             , ("git", ["add","."])
-             , ("git", ["commit","-m","\"Initial commit.\""])]
+-- should be something like
+      
+{-main = do
+    cd locaGitDir
+    procEmpty ("stack", ["new", repoName, "simple"])
+    cd repoName
+    map procEmpty [ ("stack", ["setup"])
+                  , ("git",   ["init"])
+                  , ("git",   ["add",    "."])
+                  , ("git",   ["commit", "-m",     "\"Initial commit.\""])
+                  , ("git",   ["remote", "add",    "origin",                gitHubSSHString])
+                  , ("hub",   ["create", "-d",     "foo"])
+                  , ("git",   ["push",   "origin", "master"])]
 -}
-  
