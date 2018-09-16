48bb599ec534d97db277b93b79838fac9c09b4f8
Jud Taylor <jud.taylor@gmail.com>
2018-09-15 21:43:43 -0400

    Used mapM_ to map procEmpty over (cmd,[arg]) tuples.

--- src/Main.hs
+++ src/Main.hs
@@ -1,12 +1,11 @@
 {-# LANGUAGE OverloadedStrings #-}
 
--- TODO:
--- enable passing arguments from command line (github user id, name of project/repo to build)
---[x] build string (:: IsString Text) for remote URL
--- Study how to combine let and do
--- make a function like proc that takes one 2-tuple (command, [arg]) and evaluates to
+-- TODO: enable passing arguments from command line (github user id, name of project/repo to build)
+-- DONE: build string (:: IsString Text) for remote URL
+-- TODO: Study how to combine let and do
+-- DONE: make a function like proc that takes one 2-tuple (command, [arg]) and evaluates to
 --      proc command [arg] empty
--- then map that fn over a list of 2-tuples to consolidate many lines of proc...
+-- DONE: then map that fn over a list of 2-tuples to consolidate many lines of proc...
 -- DONE: learn how to convert a String to a Text --- import qualified Data.Text as T //  (T.pack string)
 
 module Main where
@@ -34,16 +33,20 @@ tempty = Turtle.empty
 procEmpty :: MonadIO io => (ProcCmd, [ProcArg]) -> io ExitCode
 procEmpty (comd, args) = proc comd args tempty
   
--- I'd like to replace many lines in the main block with something like the following:)
---map procEmpty [ ("stack", ["setup"])
---              , ("git", ["init"]),
---              , ("git", ["add","."])
---              , ("git", ["commit","-m","\"Initial commit.\""])]
--- Question: map will collect a structure of results of applying its function to each set of data in a node of map's target.
--- I need to create something that can be returned.
--- I will try using either mapM_ or mapM
-  
+main = do
+    cd $ fromString localGitDir
+    procEmpty  ("stack", ["new", repoName, "simple"])
+    cd $ fromString ("./" ++ (unpack repoName) ++ "/")
+    mapM_ procEmpty [ ("stack", ["setup"])
+                    , ("git", ["init"]) 
+                    , ("git", ["add", "."])
+                    , ("git", ["commit", "-m", "\"Initial commit.\""])
+                    , ("git", ["remote", "add", "origin", Data.Text.concat ["git@github.com:", githubUserName, "/", repoName] ])
+                    , ("hub", ["create", "-d", repoName])
+                    , ("git", ["push", "origin", "master"]) ]
 
+
+{-
 -- this one works  
 main = do
     cd $ fromString localGitDir
@@ -57,7 +60,10 @@ main = do
     procEmpty ("hub", ["create", "-d", repoName])
     procEmpty ("git", ["push", "origin", "master"])
 
+-}
+  
 {-
+
 -- this one works  
 main = do
     cd $ fromString localGitDir
@@ -70,25 +76,17 @@ main = do
     proc "git" ["remote", "add", "origin", Data.Text.concat ["git@github.com:", githubUserName, "/", repoName] ] tempty
     proc "hub" ["create", "-d", repoName] tempty
     proc "git" ["push", "origin", "master"] tempty 
--}
-
-
-      
-
-
-
-      
+   
 -- should be something like
       
-{-main = do
+main = do
     cd locaGitDir
     procEmpty ("stack", ["new", repoName, "simple"])
     cd repoName
-    map procEmpty [ ("stack", ["setup"])
+    mapM_ procEmpty [ ("stack", ["setup"])
                   , ("git",   ["init"])
                   , ("git",   ["add",    "."])
                   , ("git",   ["commit", "-m",     "\"Initial commit.\""])
                   , ("git",   ["remote", "add",    "origin",                gitHubSSHString])
                   , ("hub",   ["create", "-d",     "foo"])
-                  , ("git",   ["push",   "origin", "master"])]
--}
+                  , ("git",   ["push",   "origin", "master"])] -}
