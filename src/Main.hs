3f152eb0d9d4aee20ec75d9d02b495a442cd98fc
Jud Taylor <jud.taylor@gmail.com>
2018-09-17 17:49:34 -0400

    Integrated command line argumet->parameter passing.

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,6 @@
-
 {-# LANGUAGE OverloadedStrings #-}
 
--- TODO: enable passing arguments from command line (github user id, name of project/repo to build)
+-- DONE: enable passing arguments from command line (github user id, name of project/repo to build)
 -- DONE: build string (:: IsString Text) for remote URL
 -- DONE: Study how to combine let and do
 -- DONE: make a function like proc that takes one 2-tuple (command, [arg]) and evaluates to
@@ -13,6 +12,33 @@ module Main where
 
 import Turtle
 import Data.Text 
+import Options.Applicative
+import Data.Semigroup ( (<>) )
+
+data NewProjectParams = NewProjectParams
+  { repoName       :: String
+  , githubUserName :: String
+  , localGitDir    :: String }
+
+paramsParser :: Parser NewProjectParams
+paramsParser = NewProjectParams
+      <$> strOption
+          ( long "project-repo-name"
+         <> short 'r'
+         <> metavar "REPONAME"
+         <> help "Name to attach to new project and repo." )
+      <*> strOption
+          ( long "github-name"
+         <> short 'g'
+         <> metavar "GITHUB-NAME"
+         <> value "gptix"
+         <> help "User name at github.com." )
+      <*> strOption
+          ( long "local-github-directory"
+         <> short 'l'
+         <> metavar "LOCAL-DIR"
+         <> help "Path to local directory for project."
+         <> value "/home/gt/gitstuff" )
 
 type ProcCmd = Text
 type ProcArg = Text
@@ -20,43 +46,33 @@ type ProcArg = Text
 procEmpty :: MonadIO io => (ProcCmd, [ProcArg]) -> io ExitCode
 procEmpty (comd, args) = proc comd args Turtle.empty
 
-{-  
-main =
-  -- I want these three variables to be populated by values passed by a command line parser.
-  let localGitDir    = (  "/home/gt/gitstuff" :: String)
-      repoName       = (  "test-optparse"               :: Text) 
-      githubUserName = (  "gptix"             :: Text) in 
+ 
+main :: IO ()
+main = createStackProject =<< execParser opts
+  where
+    opts = info (paramsParser <**> helper)
+      ( fullDesc
+     <> progDesc "Create stack project; initialize git; create repo on github.com; add/commit/push."
+     <> Options.Applicative.header "A stack project and git repo maker/sync'er." )
+
+
+createStackProject :: MonadIO io => NewProjectParams -> io ()
+createStackProject (NewProjectParams repoName githubUserName localGitDir) =
+  let rText = pack repoName
+      gText = pack githubUserName in
+
     do
+  
       cd $ fromString localGitDir
-      procEmpty  ("stack", ["new", repoName, "simple"])
-      cd $ fromString ("./" ++ (unpack repoName) ++ "/")
+      procEmpty  ("stack", ["new", rText, "simple"])
+      cd $ fromString ("./" ++ repoName ++ "/")
       mapM_ procEmpty [ ("stack", ["setup"])
                       , ("git", ["init"]) 
                       , ("git", ["add", "."])
                       , ("git", ["commit", "-m", "\"Initial commit.\""])
-                      , ("git", ["remote", "add", "origin", Data.Text.concat ["git@github.com:", githubUserName, "/", repoName] ])
-                      , ("hub", ["create", "-d", repoName])
+                      , ("git", ["remote", "add", "origin", Data.Text.concat ["git@github.com:", gText, "/", rText] ])
+                      , ("hub", ["create", "-d", rText])
                       , ("git", ["push", "origin", "master"]) ]
--}
-
-main = let l = ( "/home/gt/gitstuff" :: String)
-           r = (  "foo"              :: Text) 
-           g = (  "gptix"            :: Text) in
-           createStackProject l r g
-           
-
-createStackProject :: MonadIO io => String -> Text -> Text -> io ()
-createStackProject localGitDir repoName githubUserName = do
-        cd $ fromString localGitDir
-        procEmpty  ("stack", ["new", repoName, "simple"])
-        cd $ fromString ("./" ++ (unpack repoName) ++ "/")
-        mapM_ procEmpty [ ("stack", ["setup"])
-                        , ("git", ["init"]) 
-                        , ("git", ["add", "."])
-                        , ("git", ["commit", "-m", "\"Initial commit.\""])
-                        , ("git", ["remote", "add", "origin", Data.Text.concat ["git@github.com:", githubUserName, "/", repoName] ])
-                        , ("hub", ["create", "-d", repoName])
-                        , ("git", ["push", "origin", "master"]) ]
   
 
 
@@ -74,6 +90,26 @@ createStackProject localGitDir repoName githubUserName = do
 
 -- tempty = Turtle.empty
 
+
+{-  
+-- this one works
+main =
+  -- I want these three variables to be populated by values passed by a command line parser.
+  let localGitDir    = (  "/home/gt/gitstuff" :: String)
+      repoName       = (  "test-optparse"               :: Text) 
+      githubUserName = (  "gptix"             :: Text) in 
+    do
+      cd $ fromString localGitDir
+      procEmpty  ("stack", ["new", repoName, "simple"])
+      cd $ fromString ("./" ++ (unpack repoName) ++ "/")
+      mapM_ procEmpty [ ("stack", ["setup"])
+                      , ("git", ["init"]) 
+                      , ("git", ["add", "."])
+                      , ("git", ["commit", "-m", "\"Initial commit.\""])
+                      , ("git", ["remote", "add", "origin", Data.Text.concat ["git@github.com:", githubUserName, "/", repoName] ])
+                      , ("hub", ["create", "-d", repoName])
+                      , ("git", ["push", "origin", "master"]) ]
+-}
 
 
 {-
